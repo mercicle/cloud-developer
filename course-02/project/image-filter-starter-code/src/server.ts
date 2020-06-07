e905c9d8d0157338ee9cf2dd33229b85dcff00e7
John Mercer <presci3nce@gmail.com>
2020-06-07 15:32:04 -0400

    Image filter app initial implementation working locally. Next going to deploy to elastic beanstalk.

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -9,7 +9,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -30,13 +30,67 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+
+  // USING udacity-api-restapi AS TEMPLATE:
+  // router.get('/signed-url/:fileName',
+  //     requireAuth,
+  //     async (req: Request, res: Response) => {
+  //     let { fileName } = req.params;
+  //     const url = AWS.getPutSignedUrl(fileName);
+  //     res.status(201).send({url: url});
+  // });
+
+  app.get( "/filteredimage", async ( req, res ) => {
+
+    var image_url = req.query.image_url;
+    if (!image_url) {
+      return res.status(400).send(`id is required`);
+    }
+
+    //this helped alot!
+    //https://stackoverflow.com/questions/59134016/simple-api-endpoint-in-nodejs
+    var myAsyncFilter = async function (in_url:string) {
+      try {
+        var filtered_path = await filterImageFromURL(in_url);
+        return filtered_path;
+      } catch(err) {
+        return "Error";
+      }
+    };
+
+    const filtered_image = await myAsyncFilter(image_url);
+
+    // https://knowledge.udacity.com/questions/151148
+    // this helped alot!
+    res.status(200).sendFile(filtered_image , () => deleteLocalFiles([filtered_image]));
+
+  });
+
+  // @image_url: a publicly available url to an image
+  // app.get("/filteredimage/:image_url", async(req : Request, res: Response) => {
+  //
+  //           let { image_url } = req.query;
+  //           console.log("Image url: "+image_url);
+  //           //const image_url = req.query.image_url; //<-- what is the diff between using these two options?
+  //
+  //           if(!image_url){
+  //             // status code 400 is bad request
+  //             // https://www.restapitutorial.com/httpstatuscodes.html
+  //             res.status(400).send("image_url parameter is missing");
+  //           }else{
+  //             const filtered_image = await filterImageFromURL(image_url);
+  //             //res.sendFile(filtered_image);
+  //             return res.status(200).send(`Welcome to the Cloud, ${image_url}!`);
+  //             //deleteLocalFiles([filtered_image]);
+  //           }
+  // })
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
-  
+
 
   // Start the Server
   app.listen( port, () => {
