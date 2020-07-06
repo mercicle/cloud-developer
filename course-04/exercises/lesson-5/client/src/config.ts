908774c4669ffcf12a315250edbb2288d91843ea
John Mercer <presci3nce@gmail.com>
2020-07-05 22:08:18 -0400

    npm install error documentation

--- course-04/exercises/lesson-5/client/src/config.ts
+++ course-04/exercises/lesson-5/client/src/config.ts
@@ -2,6 +2,6 @@ export const apiEndpoint = 'https://76g0hfoa7l.execute-api.us-east-2.amazonaws.c
 
 export const authConfig = {
   domain: 'dev-aqymclse.us.auth0.com',
-  clientId: 'bu2uT6QRPAMCxEgWCVjE34rcwD21vBer',
+  clientId: 'Yr1iMtzDP8rc9Bc7xYcTvdvXrxE3Qn4W',
   callbackUrl: 'http://localhost:3000/callback'
 }
