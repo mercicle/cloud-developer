4a2350d32b372111903571aaeff349ff422db874
John Mercer <presci3nce@gmail.com>
2020-07-13 18:08:13 -0400

    Implementing the generateSignedUrl.ts and createTodo.ts and updating serverless.yaml config

--- serverless/project/completed-project/backend/src/lambda/http/createTodo.ts
+++ serverless/project/completed-project/backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,31 @@
-// import 'source-map-support/register'
-//
-// import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-//
-// import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-//
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const newTodo: CreateTodoRequest = JSON.parse(event.body)
-//
-//   // TODO: Implement creating a new TODO item
-//   return undefined
-// }
+import 'source-map-support/register'
+
+import * as uuid from 'uuid'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { getUserId } from '../utils'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+const STATUS_CREATED = 201
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+
+  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  const userID = getUserId(event)
+  const todoID = uuid.v4()
+
+  console.log(`Creating TODO: ${todoID} for User ID: ${userID}`)
+
+  const newItem = { todoId: todoID, userId: userID, ...newTodo }
+
+  await docClient.put({TableName: todosTable, Item: newItem}).promise()
+
+  return {statusCode: STATUS_CREATED,
+          headers: {'Access-Control-Allow-Origin': '*'},
+          body: JSON.stringify({ newItem })
+         }
+
+}
--- serverless/project/completed-project/backend/src/lambda/http/generateUploadUrl.ts
+++ serverless/project/completed-project/backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,42 @@
-// import 'source-map-support/register'
-//
-// import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-//
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
-//
-//   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-//   return undefined
-// }
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+
+import * as AWS from 'aws-sdk'
+import * as uuid from 'uuid'
+
+const todosTable = process.env.TODOS_TABLE
+const bucketName = process.env.TODO_IMAGES_S3_BUCKET
+const urlExpiration = parseInt(process.env.SIGNED_URL_EXPIRATION)
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const s3 = new AWS.S3({signatureVersion: 'v4'})
+const STATUS_CREATED = 201
+
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+
+  const todoID  = event.pathParameters.todoId
+  console.log(`Getting Signed URL for TODO ID: ${todoID}`)
+
+  const imageID = uuid.v4()
+  const imageURL = `https://${bucketName}.s3.amazonaws.com/${imageID}`
+  const signedURL = s3.getSignedUrl('putObject',{ Bucket: bucketName, Key: imageID, Expires: urlExpiration })
+
+  const updateUrlObject = {
+          TableName: todosTable,
+          Key: { "todoId": todoID },
+          UpdateExpression: "set attachmentUrl = :a",
+          ExpressionAttributeValues:{":a": imageURL},
+          ReturnValues:"UPDATED_NEW"
+  }
+
+  await docClient.update(updateUrlObject).promise()
+  
+  return { statusCode: STATUS_CREATED,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({ imageUrl: imageURL, uploadUrl: signedURL})
+         }
+}
