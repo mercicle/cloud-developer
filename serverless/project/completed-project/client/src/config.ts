9e2f971a027325e1419d15f6f7ae52f348e411ec
John Mercer <presci3nce@gmail.com>
2020-07-15 19:40:01 -0400

    Realized I forgot cors: true in serverless.yaml for the get handler.  Debugging getTodos Access to XMLHttpRequest at 'https://.../todos' from origin 'http://localhost:3000' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.

--- serverless/project/completed-project/client/src/config.ts
+++ serverless/project/completed-project/client/src/config.ts
@@ -1,11 +1,9 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
 const apiId = '7og2ynlrmd'
 const region = 'us-east-2'
 export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: 'dev-aqymclse.us.auth0.com', // Auth0 domain
-  clientId: 'JHOBTfVDX5CfUfukZshwyqa5gpiRBNEO',          // Auth0 client id
+  domain: 'dev-aqymclse.us.auth0.com',
+  clientId: 'JHOBTfVDX5CfUfukZshwyqa5gpiRBNEO',
   callbackUrl: 'http://localhost:3000/callback'
 }
