8a939c64d06b1ba3b440b3ae2986afd3bdb4af01
John Mercer <presci3nce@gmail.com>
2020-07-18 20:46:54 -0400

    First iteration of data layer and business logic refactoring update

--- serverless/project/completed-project/backend/src/businessLogic/todo.ts
+++ serverless/project/completed-project/backend/src/businessLogic/todo.ts
@@ -1,29 +1,74 @@
+
 import * as uuid from 'uuid'
 
-import { Group } from '../models/Group'
-import { GroupAccess } from '../dataLayer/groupsAccess'
-import { CreateGroupRequest } from '../requests/CreateGroupRequest'
-import { getUserId } from '../auth/utils'
+// models and requests
+import { TodoItem } from '../models/TodoItem'
+import { CreateTodoRequest } from '../requests/CreateTodoRequest'
+import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 
-const groupAccess = new GroupAccess()
+// data layer
+import { TodoAccess } from '../dataLayer/todosAccess'
 
-export async function getAllGroups(): Promise<Group[]> {
-  return groupAccess.getAllGroups()
+// load the data interface
+const todoAccess = new TodoAccess()
+
+
+export async function createTodo(createTodoRequest: CreateTodoRequest, userId: string): Promise<TodoItem>{
+    const createdAt = new Date().toISOString()
+    const todoId = uuid.v4()
+    const createObject = {userId: userId, todoId: todoId,  name: createTodoRequest.name, createdAt: createdAt, dueDate: createTodoRequest.dueDate, done: false}
+    return await todoAccess.createTodo(createObject)
 }
 
-export async function createGroup(
-  createGroupRequest: CreateGroupRequest,
-  jwtToken: string
-): Promise<Group> {
-
-  const itemId = uuid.v4()
-  const userId = getUserId(jwtToken)
-
-  return await groupAccess.createGroup({
-    id: itemId,
-    userId: userId,
-    name: createGroupRequest.name,
-    description: createGroupRequest.description,
-    timestamp: new Date().toISOString()
-  })
+export async function updateTodo(updateTodoRequest: UpdateTodoRequest, userId: string, todoId: string): Promise<TodoItem>{
+    const updateObject = {userId: userId, todoId: todoId, name: updateTodoRequest.name, dueDate: updateTodoRequest.dueDate, done: updateTodoRequest.done }
+    return await todoAccess.updateTodo(updateObject)
 }
+
+export async function updateTodoUrl(updateTodo, userId: string, todoId: string): Promise<TodoItem>{
+    const updateObject = { userId: userId, todoId: todoId, name: updateTodo.name, attachmentUrl: updateTodo.attachmentUrl }
+    return await todoAccess.updateTodoUrl(updateObject)
+}
+
+export async function removeTodo(userId: string, todoId: string){
+    return await todoAccess.removeTodo(userId, todoId)
+}
+
+export async function getTodo(userId: string, todoId: string): Promise<TodoItem[]>{
+    return todoAccess.getTodo(userId, todoId)
+}
+
+export async function getAllTodos(userId: string): Promise<TodoItem[]>{
+    return todoAccess.getAllTodos(userId)
+}
+
+
+// import * as uuid from 'uuid'
+//
+// import { Group } from '../models/Group'
+// import { GroupAccess } from '../dataLayer/groupsAccess'
+// import { CreateGroupRequest } from '../requests/CreateGroupRequest'
+// import { getUserId } from '../auth/utils'
+//
+// const groupAccess = new GroupAccess()
+//
+// export async function getAllGroups(): Promise<Group[]> {
+//   return groupAccess.getAllGroups()
+// }
+//
+// export async function createGroup(
+//   createGroupRequest: CreateGroupRequest,
+//   jwtToken: string
+// ): Promise<Group> {
+//
+//   const itemId = uuid.v4()
+//   const userId = getUserId(jwtToken)
+//
+//   return await groupAccess.createGroup({
+//     id: itemId,
+//     userId: userId,
+//     name: createGroupRequest.name,
+//     description: createGroupRequest.description,
+//     timestamp: new Date().toISOString()
+//   })
+// }
--- serverless/project/completed-project/backend/src/dataLayer/todosAccess.ts
+++ serverless/project/completed-project/backend/src/dataLayer/todosAccess.ts
@@ -1,40 +1,97 @@
-import * as AWS  from 'aws-sdk'
-import * as AWSXRay from 'aws-xray-sdk'
+
+import * as AWS from 'aws-sdk'
+const AWSXRay = require('aws-xray-sdk')
+const XAWS = AWSXRay.captureAWS(AWS)
+import { TodoItem } from '../models/TodoItem'
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 
-const XAWS = AWSXRay.captureAWS(AWS)
+export class TodoAccess {
 
-import { Group } from '../models/Group'
+    constructor(
+        private readonly docClient: DocumentClient = createDynamoDBClient(),
+        private readonly todoTable = process.env.TODOS_TABLE,
+        private readonly todoIndex = process.env.INDEX_NAME){}
 
-export class GroupAccess {
+    async createTodo(todo: TodoItem): Promise<TodoItem> {
 
-  constructor(
-    private readonly docClient: DocumentClient = createDynamoDBClient(),
-    private readonly groupsTable = process.env.GROUPS_TABLE) {
-  }
+        const putObject = { TableName: this.todoTable, Item: todo}
 
-  async getAllGroups(): Promise<Group[]> {
-    console.log('Getting all groups')
+        await this.docClient.put(putObject).promise()
 
-    const result = await this.docClient.scan({
-      TableName: this.groupsTable
-    }).promise()
+        return todo
 
-    const items = result.Items
-    return items as Group[]
-  }
+    }
 
-  async createGroup(group: Group): Promise<Group> {
-    await this.docClient.put({
-      TableName: this.groupsTable,
-      Item: group
-    }).promise()
+    async updateTodo(updatedTodo: TodoItem): Promise<TodoItem> {
 
-    return group
-  }
+        // name is a reserved keyword: https://knowledge.udacity.com/questions/201486
+        const dynamoQuery = {
+
+          TableName: this.todoTable,
+          IndexName: this.todoIndex,
+          Key: { todoId: updatedTodo.todoId , userId: updatedTodo.userId},
+          ExpressionAttributeNames: { "#N": "name" },
+          UpdateExpression: "set #N=:todoName, dueDate =:dueDate, done =:done",
+          ExpressionAttributeValues: { ":todoName": updatedTodo.name, ":dueDate": updatedTodo.dueDate, ":done": updatedTodo.done},
+          ReturnValues: "UPDATED_NEW"
+
+        }
+
+        await this.docClient.update(dynamoQuery).promise()
+
+        return updatedTodo
+
+    }
+
+    async updateTodoUrl(updatedTodo: TodoItem): Promise<TodoItem> {
+
+        const dynamoQuery = {
+                TableName: this.todoTable,
+                IndexName: this.todoIndex,
+                Key: { todoId: updatedTodo.todoId , userId: updatedTodo.userId},
+                UpdateExpression: "set attachmentUrl = :a",
+                ExpressionAttributeValues:{":a": updatedTodo.attachmentUrl},
+                ReturnValues:"UPDATED_NEW"
+        }
+
+        await this.docClient.update(dynamoQuery).promise()
+
+        return updatedTodo
+
+    }
+
+    async removeTodo(userID: string, todoID: string) {
+
+        const deleteQuery = { TableName: this.todoTable, Key: { todoId: todoID, userId: userID } }
+
+        await this.docClient.delete(deleteQuery).promise()
+
+    }
+
+    async getTodo(userId: string, todoId: string): Promise<TodoItem[]> {
+
+      const getQuery = {TableName: this.todoTable, KeyConditionExpression: 'userId = :userId AND todoId = :todoId', ExpressionAttributeValues: {':userId': userId, ':todoId': todoId}}
+      const result = await this.docClient.query(getQuery).promise()
+      const items = result.Items
+
+      return items
+
+    }
+
+    async getAllTodos(userId: string): Promise<TodoItem[]> {
+
+        const getAllQuery = { TableName: this.todoTable, KeyConditionExpression: 'userId = :userId',ExpressionAttributeValues: { ':userId': userId }}
+        const result = await this.docClient.query(getAllQuery).promise()
+        const items = result.Items
+
+        return items
+
+    }
+
 }
 
 function createDynamoDBClient() {
+
   if (process.env.IS_OFFLINE) {
     console.log('Creating a local DynamoDB instance')
     return new XAWS.DynamoDB.DocumentClient({
@@ -45,3 +102,52 @@ function createDynamoDBClient() {
 
   return new XAWS.DynamoDB.DocumentClient()
 }
+
+// from lesson
+// import * as AWS  from 'aws-sdk'
+// import * as AWSXRay from 'aws-xray-sdk'
+// import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+//
+// const XAWS = AWSXRay.captureAWS(AWS)
+//
+// import { Group } from '../models/Group'
+//
+// export class GroupAccess {
+//
+//   constructor(
+//     private readonly docClient: DocumentClient = createDynamoDBClient(),
+//     private readonly groupsTable = process.env.GROUPS_TABLE) {
+//   }
+//
+//   async getAllGroups(): Promise<Group[]> {
+//     console.log('Getting all groups')
+//
+//     const result = await this.docClient.scan({
+//       TableName: this.groupsTable
+//     }).promise()
+//
+//     const items = result.Items
+//     return items as Group[]
+//   }
+//
+//   async createGroup(group: Group): Promise<Group> {
+//     await this.docClient.put({
+//       TableName: this.groupsTable,
+//       Item: group
+//     }).promise()
+//
+//     return group
+//   }
+// }
+//
+// function createDynamoDBClient() {
+//   if (process.env.IS_OFFLINE) {
+//     console.log('Creating a local DynamoDB instance')
+//     return new XAWS.DynamoDB.DocumentClient({
+//       region: 'localhost',
+//       endpoint: 'http://localhost:8000'
+//     })
+//   }
+//
+//   return new XAWS.DynamoDB.DocumentClient()
+// }
