004ec318d17ea0807341cb05e29ef8a5a734c26e
John Mercer <presci3nce@gmail.com>
2020-07-18 20:59:37 -0400

    Updated create service

--- serverless/project/completed-project/backend/src/lambda/http/createTodo.ts
+++ serverless/project/completed-project/backend/src/lambda/http/createTodo.ts
@@ -1,32 +1,20 @@
 import 'source-map-support/register'
-
-import * as uuid from 'uuid'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-import * as AWS from 'aws-sdk'
-//import * as AWSXRay from 'aws-xray-sdk'
-const AWSXRay = require('aws-xray-sdk')
-
-import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { getUserId } from '../utils'
+import { createTodo } from '../../businessLogic/todo'
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 
-const XAWS = AWSXRay.captureAWS(AWS)
-
-const dynamoDocClient = new XAWS.DynamoDB.DocumentClient()
-const TODOS_TABLE = process.env.TODOS_TABLE
 const STATUS_CREATED = 201
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
   const userID = getUserId(event)
-  const todoID = uuid.v4()
 
-  console.log(`Creating TODO: ${todoID} for User ID: ${userID}`)
+  const eventJSONString = JSON.parse(event.body)
+  console.log(`Creating: ${eventJSONString}`)
 
-  const newItem = { todoId: todoID, userId: userID, ...newTodo }
-
-  //https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
-  await dynamoDocClient.put({TableName: TODOS_TABLE, Item: newItem}).promise()
+  const newItem = await createTodo(newTodo, userID)
 
   return {statusCode: STATUS_CREATED,
           headers: {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': true },
