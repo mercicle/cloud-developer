ac9b6174a7c9c569f375870b2fc68aa6788e6297
John Mercer <presci3nce@gmail.com>
2020-07-23 14:40:49 -0400

    restructuring for final project.

--- final-project/backend/src/lambda/dynamoDb/elasticSearchSync.ts
+++ final-project/backend/src/lambda/dynamoDb/elasticSearchSync.ts
@@ -14,7 +14,9 @@ export const handler: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent)
   console.log('Processing events batch from DynamoDB', JSON.stringify(event))
 
   for (const record of event.Records) {
+
     console.log('Processing record', JSON.stringify(record))
+    
     if (record.eventName !== 'INSERT') {
       continue
     }
